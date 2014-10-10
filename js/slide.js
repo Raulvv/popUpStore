3238ddaae932ef65d06839e90d40dc6d7553b18c
Raul Vega <raulvprivate@gmail.com>
2014-10-10 12:34:20 +0200

    Slide hecho con Javascript solo, sin JQuery

--- js/slide.js
+++ js/slide.js
@@ -1,18 +1,35 @@
-$(document).ready(function(){
+/*var varCounter = 0;
+var varName = function(){
+     if(varCounter <= 6) {
+          varCounter++;
+          
+     } else {
+          clearInterval(varName);
+     }
+};*/
+
+window.onload= function(){
     var contador=1;
 
-    for (var i = 1; i <= 6; i++) {
-        $("#img"+i).hide();
-    };
-    
-    $("#img1").show()
-
-    $(".control-next").click(function(){
-        pasarImagen();
-    })
+    document.getElementById("img1").style.display="inline";
 
     function pasarImagen(){
-        $("#img"+contador).hide();
+        var img=document.getElementById("img"+contador);
+        img.style.zIndex= "0"
+        img.style.animation= "slide 1s ease infinite normal";
+        
+        if(contador<6){
+            contador++;
+        }
+        else{
+            contador=1;
+        }
+        var imgNext=document.getElementById("img"+contador)
+        imgNext.style.zIndex= "-3"
+        imgNext.style.display= "inline";
+        setTimeout(function(){img.style.display= "none";
+        img.style.animation= "";},1000);
+        /*document.getElementById("img"+contador).style.display= "none";
         contador++;
         $("#img"+contador).fadeIn();
 
@@ -20,8 +37,8 @@ $(document).ready(function(){
             $("#img"+contador).hide();
             contador=1;   
             $("#img"+contador).fadeIn();
-        }
+        }*/
     }
 
     window.setInterval(pasarImagen, 5000);
-});
+}
