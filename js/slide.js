c8c9cfa11573744d901aae576bc3eed2752bdc55
Raul Vega <raulvprivate@gmail.com>
2014-10-10 12:45:32 +0200

    codigo limpio slide javascript

--- js/slide.js
+++ js/slide.js
@@ -1,13 +1,3 @@
-/*var varCounter = 0;
-var varName = function(){
-     if(varCounter <= 6) {
-          varCounter++;
-          
-     } else {
-          clearInterval(varName);
-     }
-};*/
-
 window.onload= function(){
     var contador=1;
 
@@ -24,20 +14,12 @@ window.onload= function(){
         else{
             contador=1;
         }
+        
         var imgNext=document.getElementById("img"+contador)
         imgNext.style.zIndex= "-3"
         imgNext.style.display= "inline";
         setTimeout(function(){img.style.display= "none";
         img.style.animation= "";},1000);
-        /*document.getElementById("img"+contador).style.display= "none";
-        contador++;
-        $("#img"+contador).fadeIn();
-
-        if(contador==6){
-            $("#img"+contador).hide();
-            contador=1;   
-            $("#img"+contador).fadeIn();
-        }*/
     }
 
     window.setInterval(pasarImagen, 5000);
