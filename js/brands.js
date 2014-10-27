28dd2de119a827d1b01694ec5a3a03ec52da04d3
Raul Vega <raulvprivate@gmail.com>
2014-10-27 16:06:41 +0100

    correcciones minimas.

--- js/brands.js
+++ js/brands.js
@@ -1,3 +1,4 @@
+//Hash for Brands
 var brands = [
 {key:"Mr. Boho",
 value: "img_1"},
@@ -18,36 +19,27 @@ for (var i=0;i<links.length;i++)
 }
 
 function pressBrand(event) {
-	console.log("1")
 	var encontrado;
 	var x= 0;
 	var images = 5;
-	console.log("2")
+
 	for (var i=1;i<=images;i++)
 	{
-		console.log("3")
 		document.getElementById("img_"+i).style.display="none";
 	}
 	
 	var marca=event.target.firstChild.nodeValue;
-	var imgBrand
-	console.log("4")
+	var imgBrand;
 
 	while(x<brands.length&&!encontrado)
 	{
-		console.log("1")
 		if (brands[x].key==marca)
 		{
-			
 			imgBrand=document.getElementById(brands[x].value);
-			console.log(brands[x].value)
 			encontrado=true;
 		}
 		x++;
 	}
-
-	var numImg=marca.split(" ");
-
 	imgBrand.style.display="inline";
 }
 
